77a4a375d941e5775319569f07251c379b560c00
Jenny Paul <jenny@thepauls.org>
2020-11-11 14:18:14 -0600

    added in total function

--- front-end/src/quote-table.js
+++ front-end/src/quote-table.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "./Page1.css";
 import axios from "axios";
 import {
@@ -9,29 +9,19 @@ import {
   Form,
 } from "react-bootstrap";
 
-//const _defaultQuote = [
-  //{
-    //quote_name: " ",
-//    discount: 0,
-  //  description: " ",
-    //secret_note: " ",
-  //  customer_email: " "
-//  }
-//];
-
 export default class QuoteTable extends React.Component {
   state = {
     quote_name: " ",
     price: 0,
     discount: 0,
-    description: "default",
-    secret_notes: "default",
-    customer_email: "default"
+    description: "",
+    secret_notes: "",
+    customer_email: "",
+    total:0
   }
 
-handleChange = event => {
-  event.preventDefault();
-
+handleInChange = event => {
+    event.preventDefault();
 //handle quote_name
   const quote_name = {
     quote_name: this.state.quote_name
@@ -61,44 +51,49 @@ handleChange = event => {
       price: this.state.customer_email
     };
 
-//the following blocks of code handle the post requests
-  axios.post('http://blitz.cs.niu.edu/PurchaseOrder/', {quote_name})
-    .then(res => {
+  //the following blocks of code handle the post requests
+    axios.put("http://blitz.cs.niu.edu/PurchaseOrder/", {quote_name})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+
+    axios.put("http://blitz.cs.niu.edu/PurchaseOrder/", {price})
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+
+    axios.put("http://blitz.cs.niu.edu/PurchaseOrder/", {discount})
+      .then(res => {
       console.log(res);
       console.log(res.data);
-    })
+      })
 
-  axios.post('http://blitz.cs.niu.edu/PurchaseOrder/', {price})
-    .then(res => {
+    axios.put("http://blitz.cs.niu.edu/PurchaseOrder/", {description})
+      .then(res => {
       console.log(res);
       console.log(res.data);
-    })
+      })
 
-  axios.post('http://blitz.cs.niu.edu/PurchaseOrder/', {discount})
-    .then(res => {
-    console.log(res);
-    console.log(res.data);
-    })
+    axios.put("http://blitz.cs.niu.edu/PurchaseOrder/", {secret_notes})
+      .then(res => {
+      console.log(res);
+      console.log(res.data);
+      })
 
-  axios.post('http://blitz.cs.niu.edu/PurchaseOrder/', {description})
-    .then(res => {
-    console.log(res);
-    console.log(res.data);
-    })
-
-  axios.post('http://blitz.cs.niu.edu/PurchaseOrder/', {secret_notes})
-    .then(res => {
-    console.log(res);
-    console.log(res.data);
-    })
-
-  axios.post('http://blitz.cs.niu.edu/PurchaseOrder/', {customer_email})
-    .then(res => {
-    console.log(res);
-    console.log(res.data);
-    })
+    axios.put("http://blitz.cs.niu.edu/PurchaseOrder/", {customer_email})
+      .then(res => {
+      console.log(res);
+      console.log(res.data);
+      })
 }
 
+handleOutChange= (event) => {
+  this.setState({
+    total: this.state.price - this.state.discount
+  });
+}
 render() {
   return (
     <div className="table">
@@ -117,70 +112,70 @@ render() {
         <div className="table-body">
             <div className="table-row">
               <div className="table-data">
-                <form onSubmit={this.handleSubmit}>
+               <form onSubmit={this.handleSubmit}>
                 <input
                   type="text"
                   name="quote_name"
-                  onChange={this.handleChange}
+                  onChange={this.handleInChange}
                 />
-                </form>
+              </form>
               </div>
               <div className="table-data">
-              <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                name="price"
-                onChange={this.handleChange}
-              />
-              </form>
+               <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  name="price"
+                  onChange={this.handleInChange}
+                  />
+                </form>
               </div>
             </div>
           <div className="table-row">
             <div className="table-data">
               <div>Discount:</div>
-              <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                name="discount"
-                onChange={this.handleChange}
-              />
-              </form>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                  type="text"
+                  name="discount"
+                  onChange={this.handleInChange}
+                  />
+                </form>
             </div>
           </div>
           <div className="table-row">
             <div className="table-data">
               <div>Description:</div>
-              <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                name="description"
-                onChange={this.handleChange}
-              />
-              </form>
+                <form onSubmit={this.handleSubmit}>
+                <input
+                  type="text"
+                  name="description"
+                  onChange={this.handleInChange}
+                  />
+                </form>
             </div>
           </div>
           <div className="table-row">
             <div className="table-data">
               <div>Secret Notes:</div>
-              <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                name="secret_notes"
-                onChange={this.handleChange}
-              />
-              </form>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                  type="text"
+                  name="secret_notes"
+                  onChange={this.handleInChange}
+                  />
+                </form>
             </div>
           </div>
           <div className="table-row">
             <div className="table-data">
               <div>Customer Email:</div>
-              <form onSubmit={this.handleSubmit}>
-              <input
-                type="text"
-                name="customer_email"
-                onChange={this.handleChange}
-              />
-              </form>
+                <form onSubmit={this.handleSubmit}>
+                  <input
+                  type="text"
+                  name="customer_email"
+                  onChange={this.handleInChange}
+                  />
+                </form>
             </div>
           </div>
         </div>
@@ -188,16 +183,26 @@ render() {
           <div className="table-row">
             <div className="table-data">
               <div>Total After Discount:</div>
+                <form onChange = {this.handleOutChange}>
+                </form>
+                  <p>{this.state.total}</p>
             </div>
           </div>
         </div>
       </div>
       <div className="table=row">
         <div align="centered">
-          <Button variant="danger">Finalize Quote</Button>
+          <form onSubmit={this.handleSubmit}>
+            <Button
+               variant="danger"
+               type="submit">
+               Finalize Quote
+            </Button>
+          </form>
         </div>
       </div>
     </div>
     )
   }
+
 }
