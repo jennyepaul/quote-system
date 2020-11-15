a2d1835538ddb5d60de1c5b0b0cf9a71f3a178ae
Jenny Paul <>
2020-11-15 11:46:31 -0600

    changed port number on server

--- back-end/server.js
+++ back-end/server.js
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
 });
 
 // set port, listen for requests
-app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+app.listen(3001, () => {
+  console.log("Server is running on port 3001.");
 });
