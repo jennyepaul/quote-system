5ae8a6a14d498c79c5906d15ce59776625f3872f
Jenny Paul <>
2020-11-15 12:34:39 -0600

    making controller functions

---
+++ back-end/app/controller/customer.controller.js
@@ -0,0 +1,12 @@
+//written by Jennifer Paul: 11/15/20
+const Customer = require("../models/customer.model.js");
+
+// Retrieve all Customers from the database.
+exports.getAll = (req, res) => {
+
+};
+
+// Find a single Customer with a customerId
+exports.findById = (req, res) => {
+
+};
---
+++ back-end/app/controller/parts.controller.js
@@ -0,0 +1,22 @@
+//written by Jennifer Paul: 11/15/20
+const Parts = require("../models/parts.model.js");
+
+// Create and Save a new part
+exports.create = (req, res) => {
+
+};
+
+// Retrieve all parts from database
+exports.getAll = (req, res) => {
+
+};
+
+// Find a single part by part number
+exports.findByNumber = (req, res) => {
+
+};
+
+// Update a part identified by the number of the part
+exports.updateByNumber = (req, res) => {
+
+};
