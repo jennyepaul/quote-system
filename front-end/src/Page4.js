794ca0fd493c23f90ea7c325d53a5d96f6c2d068
Casey McDermott <casey0mcd@gmail.com>
2020-11-23 11:21:31 -0600

    First Attempt Page 4

---
+++ front-end/src/Page4.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import QuoteTable4 from "./quote-table4.js";
+import "./Page4.css";
+
+function App() {
+  return (
+    <div className="Title">
+      <h1>ABC PLANT PARTS</h1>
+      <div className="Page">
+        <QuoteTable4 />
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ front-end/src/quote-table4.js
@@ -0,0 +1,107 @@
+import React from "react";
+import "./Page4.css";
+import axios from "axios";
+//import { Button } from "react-bootstrap";
+//import emailjs from "emailjs-com";
+
+class QuoteTable4 extends React.Component {
+  constructor(props) {
+    super(props);
+    state = {
+      query: '',
+    results: {},
+    filteredDate: []	    
+    };
+	
+    handleInputChange = event => {
+	    const query = event.target.value;
+
+	    this.setState(prevState=> {
+		    const filteredData = prevState.data.filter(element => {
+			    return element.name.toLowerCase().includes(query.toLowerCase());
+		    });
+
+		    return {
+			    query,
+			    filteredData
+ 	 };
+       });
+    };
+
+    getData = () => {
+      axios.get('http://localhost:3001/quote/')
+	.then(res => //I dont know what to put here 
+	.then(data => {
+		const { query } = this.state;
+		const filteredData = data.filter(element => {
+			return element.name.toLowerCase().includes(query.toLowerCase());
+		});
+
+		this.setState({
+			data,
+			filteredData
+		});
+	});
+    };
+    
+    componentWillMount() {
+ 	   this.getData();
+    }
+
+render() {
+	return (
+	
+	 	<div className ="table">
+		 <div className="table-title">
+		 Search Sales Associate and Quote Information
+		 </div>
+		  <div className="table-content">
+		   <div className="table-header">
+		    <div className="table-row">
+		     <div className="table-data">
+		      <div> 
+		      Type Sales Associate ID to Retrieve Sales Associate Information
+		      </div>
+		     </div>
+		     <div className="table-data">
+                        <label className="search-label" htmlFor="search-input">
+
+		        <input
+		        	type="text"
+		                value={this.state.query}
+				id="search-input"
+				placeholder="Search..."
+				onChange={this.handleOnInputChange}
+		       />
+		       </label>
+		       <div>{this.state.filteredData.map(i=> <p>{i.name}</p>)}</div>
+		     </div>
+		    </div>
+		    <div className="table-row">
+		     <div className="table-data">
+		      <div>
+		      Type Quote ID to Retrieve Quote Information
+		      </div>
+		     </div>
+		     <div className="table-data">
+                        <label className="search-label" htmlFor="search-input">
+
+		        <input
+		        	type="text"
+		                value=""
+				id="search-input"
+				placeholder="Search..."
+				onChange={this.handleOnInputChange}
+		       />
+		       </label>
+		     </div>
+		    </div>
+		   </div>
+		  </div>
+		 </div>
+		     
+
+	)
+}
+}
+export default QuoteTable4; 
