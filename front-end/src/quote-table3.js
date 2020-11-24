4267e4a2953534fe741776d0cd00bc61b1a91210
Ryan1597 <43765660+Ryan1597@users.noreply.github.com>
2020-11-24 14:56:23 -0600

    Update quote-table3.js

--- front-end/src/quote-table3.js
+++ front-end/src/quote-table3.js
@@ -1,191 +1,219 @@
 import React from "react";
 import "./Page3.css";
 import axios from "axios";
-import { Button,} from "react-bootstrap";
+import { Button, } from "react-bootstrap";
 
 class QuoteTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    discount: 0,
-    price: 0,
-    finalprice: 0,
-    quote_name: '',
-    order: '',
-    associate: '',
-    custid: '',
-    amount: 0,
-  };
-}
+    constructor(props) {
+        super(props);
+        this.state = {
 
-//get the info/data from api
-componentDidMount = event => {
-axios.get('http://blitz.cs.niu.edu/PurchaseOrder/')
-    .then(response => {
-      console.log(response.data);
-  })
-    .catch(error => {
-      console.log(error);
-  });
-}
-
-handleSubmit = event => {
-    event.preventDefault();
-    const id = {
-        id: this.state.id
-    }
-    const discount = {
-        discount: this.state.discount
-    }
-    const associate = {
-        associate: this.state.associate
-    }
-    const custid = {
-        custid: this.state.custid
-    }
-    const amount = {
-        amount: this.state.amount
-    }
-    const order = {
-        order: this.state.order
-    }
-    const price = {
-        price: this.state.price
+            addDiscount: 0,
+            firstDiscount: 0,
+            finalPrice: 0,
+            quote: {
+                id: 0,
+                name: '',
+                secret_notes: '',
+                customer_email: '',
+                discount: 0,
+                price: 0,
+                sanctioned_unresolved: '',
+                final_price: 0,
+                description: ''
+            },
+            quote_name: '',
+            order: '',
+            associate: '',
+            custid: '',
+            amount: 0,
+        };
     }
 
-  const finalDiscount = {
-      finalDiscount: this.state.finalDiscount
-  }
-//handle final price
-  const finalPrice = {
-    finalPrice: this.state.finalPrice
-  };
-//handle quote name
-  const quote_name = {
-    quote_name: this.state.quote_name
-  };
-  //the following blocks of code handle the post/put requests
-  axios({
-    method: "put",
-    url:    "http://localhost:3001/quote/" + id,
-    data: {
-         discount: discount,
-         price: price,
-         finalPrice: finalPrice,
-    },
-  }).then(
-    (response) => {
-        console.log(response);
-        window.location.reload();
-    },
-    (error) => {
-        console.log(error);
-        }
-    );
-
-  axios({
-      method: "post",
-      url: "http://blitz.cs.niu.edu/PurchaseOrder/",
-      data: {
-        order: order,
-        associate: associate,
-        custid: custid,
-        amount: amount,
-      }
-  })
-}
-  handlePriceChange = (event) => {
-    this.setState({
-      price: event.target.value
-    },() =>{
-    this.setState({
-      amount: this.state.price - this.state.discount
-    });
-    });
-  }
-  handleDiscountChange = (event) => {
-      this.setState({
-        discount: event.target.value
-      },() => {
-      this.setState({
-        amount: this.state.price - this.state.discount
-      });
-      });
+    //get the info/data from api
+    componentDidMount = event => {
+        axios.get('http://blitz.cs.niu.edu/PurchaseOrder/')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
-render() {
-  return (
-    <div className="table">
-      <div className="table-title">Finalize Quote</div>
-      <div className="table-content">
-        <div className="table-header">
-          <div className="table-row">
-            <div className="table-data">
-              <div>Final Discount</div>
-            </div>
-            <div className="table-data">
-              <div>Final Price</div>
-            </div>
-          </div>
-        </div>
-        <div className="table-body">
-            <div className="table-row">
-              <div className="table-data">
-               <form onSubmit={this.handleSubmit}>
+    handleSubmit = event => {
+        event.preventDefault();
+
+
+
+        //the following blocks of code handle the post/put requests
+        axios.put(
+            "http://localhost:3001/quote/" + this.state.quote.id,
+            this.state.quote
+        ).then(
+            (response) => {
+                console.log(response);
+                window.location.reload();
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+
+        /*axios({
+            method: "post",
+            url: "http://blitz.cs.niu.edu/PurchaseOrder/",
+            data: {
+                order: order,
+                associate: associate,
+                custid: custid,
+                amount: amount
+            }
+        })
+            .then(response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            }); */
+    }
+
+    handleDiscountChange = (event) => {
+        this.setState({
+            addDiscount: event.target.value
+        })
+
+    }
+
+    handleQuoteIdChange = (event) => {
+        this.setState({
+            quote: { ...this.state.quote, id: event.target.value }
+        })
+    }
+
+    getQuote = () => {
+        console.log(this.state.quote.id)
+        axios({
+            method: "get",
+            url: "http://localhost:3001/quote/" + this.state.quote.id,
+
+        }).then(
+            (response) => {
+                this.setState({
+                    quote: {
+                        id: this.state.quote.id,
+                        name: response.data.name,
+                        secret_notes: response.data.secret_notes,
+                        customer_email: response.data.customer_email,
+                        discount: response.data.discount,
+                        price: response.data.price,
+                        sanctioned_unresolved: response.data.sanctioned_unresolved,
+                        final_price: response.data.final_price,
+                        description: response.data.description
+                    },
+                    firstDiscount: response.data.discount
+                })
+                console.log(response);
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    }
+
+    addDiscount = () => {
+        let final_discount = Number(this.state.addDiscount) + Number(this.state.quote.discount)
+        this.setState({
+            quote: { ...this.state.quote, discount: final_discount, final_price: this.state.quote.price - final_discount }
+        })
+
+    }
+    render() {
+        return (
+            <div className="table" >
+                <div className="table-title">Finalize Quote</div>
+                <div>Quote ID</div>
                 <input
-                  type="text"
-                  name="finalDiscount"
-                  value={this.state.finalDiscount}
-                  onChange={(event) => this.setState({finalDiscount: event.target.value})}
+                    type="number"
+                    value={this.state.quote.id}
+                    name="Quote ID"
+                    onChange={this.handleQuoteIdChange}
                 />
-                <button type="submit">Confirm</button>
-              </form>
-              </div>
-              <div className="table-data">
-               <form onSubmit={this.handleSubmit}>
-                <input
-                  type="text"
-                  name="finalPrice"
-                  value={this.state.finalPrice}
-                  onChange={this.handlePriceChange}
-                  />
-                  <button type="submit">Confirm</button>
-                </form>
-              </div>
-            </div>
-          <div className="table-row">
-            <div className="table-data">
-                <div>Quote Name</div>
-            </div>
-            <script>
-            console.log(quote_name);
-            </script>
-          </div>
-          <div className="table-row">
-            <div className="table-data">
-            </div>
-          </div>
-          <div className="table-row">
-            <div className="table-data">
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="table=row">
-        <div align="centered">
-          <div>
-          <Button
-                onClick={this.handleSubmit}
-                variant="danger" type="submit"
-               >
-                Purchase Order
+                <Button style= {{marginLeft: ".5em"}} onClick={this.getQuote}>Get Quote</Button>
+                <div className="container" style={{ marginTop: "1em", marginBottom: "1em" }}>
+                    <div className="row">
+                        <div className="col">
+                            Customer Name: {this.state.quote.name}
+                        </div>
+                        <div className="col">
+                            Customer Email: {this.state.quote.customer_email}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            Description: {this.state.quote.description}
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            Discount: ${this.state.firstDiscount}
+                        </div>
+                        <div className="col">
+                            Price: ${this.state.quote.price}
+                        </div>
+                    </div>
+                </div>
+                <div className="table-content">
+                    <div className="table-header">
+                        <div className="table-row">
+                            <div className="table-data">
+                                Add Discount
+                            </div>
+                        </div>
+                    </div>
+                    <div className="table-body">
+                        <div className="table-row">
+                            <div className="table-data">
+                                <input
+                                    type="number"
+                                    value={this.state.addDiscount}
+                                    name="discount"
+                                    onChange={this.handleDiscountChange}
+                                />
+                                <Button style= {{marginLeft: ".5em"}} onClick={this.addDiscount}>Confirm</Button>
+
+                            </div>
+                        </div>
+                        <div className="table-row">
+                            <div className="table-data">
+                                <div>Added Discount: ${this.state.addDiscount}</div>
+                            </div>
+                        </div>
+                        <div className="table-row">
+                            <div className="table-data">
+                                Final Discount: ${this.state.quote.discount}
+                            </div>
+                            <div className="table-data">
+                                Final Price: ${this.state.quote.final_price}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="table=row">
+                    <div align="centered">
+                        <div>
+                            <Button
+                                onClick={this.handleSubmit}
+                                variant="danger" type="submit"
+                            >
+                                Purchase Order
          </Button>
-          </div>
-        </div>
-      </div>
-    </div>
-    )
-  }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default QuoteTable;
